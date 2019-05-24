7b26bff60258e9cfff955df8bd5b9c1ef1036740
Sven Loman <462549@student.saxion.nl>
2019-05-24 11:16:44 +0200

    JagerController update

--- sneakerjagers/src/main/java/nl/sneakerjagers/demo/JagerController.java
+++ sneakerjagers/src/main/java/nl/sneakerjagers/demo/JagerController.java
@@ -1,5 +1,18 @@
 package nl.sneakerjagers.demo;
 
+import nl.sneakerjagers.demo.Models.Brand;
+import nl.sneakerjagers.demo.Models.Shoe;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+
+@Controller
 public class JagerController {
+
+    private ArrayList<Brand> brands = new ArrayList<>();
+    private ArrayList<Shoe> shoes = new ArrayList<>();
+
+    
+
 
 }
