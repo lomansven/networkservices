97f529431efb503469615ecdcddb95d66ede3e19
Sven Loman <462549@student.saxion.nl>
2019-05-28 11:05:26 +0200

    Added method to display Brands only, and Brands with their shoes

--- sneakerjagers/src/main/java/nl/sneakerjagers/demo/JagerController.java
+++ sneakerjagers/src/main/java/nl/sneakerjagers/demo/JagerController.java
@@ -19,27 +19,26 @@ public class JagerController {
     private ArrayList<User> users = DataProvider.users;
 
 
-
     @PostMapping("/user")
-    ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         if(user.getUsername()==null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        users.add(user);
+        DataProvider.addUser(user);
 
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PostMapping("/brands")
-    ResponseEntity<Brand> createBrand(@RequestBody Brand brand) {
+    public ResponseEntity<Brand> createBrand(@RequestBody Brand brand) {
         if(brand.getBrandName()==null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        brands.add(brand);
+        DataProvider.addBrand(brand);
 
         return new ResponseEntity<>(brand, HttpStatus.CREATED);
     }
 
     @PostMapping("/shoes")
-    ResponseEntity<Shoe> createShoe(@RequestBody Shoe shoe) {
+    public ResponseEntity<Shoe> createShoe(@RequestBody Shoe shoe) {
         if(shoe.getShoeName()==null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        shoes.add(shoe);
+        DataProvider.addShoe(shoe);
 
         return new ResponseEntity<>(shoe, HttpStatus.CREATED);
     }
@@ -47,7 +46,23 @@ public class JagerController {
     @GetMapping("/brands")
     public @ResponseBody String getBrands() {
         String retVal = "Brands: \n";
-        if(brands.size()==0) return retVal+= "No brands have been created yet... Be the first!";
+        if(brands.size()==0) {
+            return retVal += "No brands have been created yet... Be the first!";
+        }
+
+        for (Brand brand : brands) {
+            retVal+= "Brand: " + brand.getBrandName() + "\n";
+        }
+        return retVal;
+    }
+
+    @GetMapping("/brands/shoes")
+    public @ResponseBody String getBrandsWithShoes() {
+        String retVal = "Brands: \n";
+        if(brands.size()==0) {
+            return retVal += "No brands have been created yet... Be the first!";
+        }
+
         for (Brand brand : brands) {
             retVal+= "Brand: " + brand.getBrandName() + "\n";
 
